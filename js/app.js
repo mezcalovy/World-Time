8568956730dda5b9d983324503a172be20049991
mezcalovy <mezcalovy@o2.pl>
2017-06-29 22:35:33 +0200

    world time

--- js/app.js
+++ js/app.js
@@ -4,13 +4,13 @@ jQuery( document ).ready(function() {
     currentMinutes = currentSeconds + ( currentDate.getMinutes() * 60 );
     currentAnalogHours = ( currentDate.getHours() > 11 ) ? currentDate.getHours() - 12 : currentDate.getHours();
     currentHours = currentMinutes + ( currentAnalogHours * 3600 );
-    jQuery( '.hours' ).css( 'animation-delay', '-' + currentHours + 's' );
-    jQuery( '.hours-ir' ).css( 'animation-delay', '-' + (currentHours - 3600) + 's' );
-    jQuery( '.hours-jp' ).css( 'animation-delay', '-' + (21600 + currentHours) + 's' );
-    jQuery( '.hours-fr' ).css( 'animation-delay', '-' + currentHours + 's' );
-    jQuery( '.hours-ru' ).css( 'animation-delay', '-' + (currentHours + 3600) + 's' );
-    jQuery( '.minutes' ).css( 'animation-delay', '-' + currentMinutes + 's' );
-    jQuery( '.seconds' ).css( 'animation-delay', '-' + currentSeconds + 's' );
+    $( '.hours' ).css( 'animation-delay', '-' + currentHours + 's' );
+    $( '.hours-ir' ).css( 'animation-delay', '-' + (currentHours - 3600) + 's' );
+    $( '.hours-jp' ).css( 'animation-delay', '-' + (21600 + currentHours) + 's' );
+    $( '.hours-fr' ).css( 'animation-delay', '-' + currentHours + 's' );
+    $( '.hours-ru' ).css( 'animation-delay', '-' + (currentHours + 3600) + 's' );
+    $( '.minutes' ).css( 'animation-delay', '-' + currentMinutes + 's' );
+    $( '.seconds' ).css( 'animation-delay', '-' + currentSeconds + 's' );
     
     
  });
@@ -20,11 +20,9 @@ jQuery( document ).ready(function() {
  
  $(document).ready(function(){
     $("button").click(function(){
-        $("#div1").addClass("blue");
-        
-        $(".seconds").css({ '-webkit-animation': "rotationz 60s infinite linear" });
-        $(".seconds").css({ '--moz-animation': "rotationz 60s infinite linear" });
-        $(".seconds").css({ 'animation': "rotationz 60s infinite linear" });
+        $(".seconds").css({ '-webkit-animation': "rotationback 60s infinite linear" });
+        $(".seconds").css({ '--moz-animation': "rotationz 60back infinite linear" });
+        $(".seconds").css({ 'animation': "rotationz 60back infinite linear" });
         $(".seconds").css({ 'background-color': "green" });
        
     });
